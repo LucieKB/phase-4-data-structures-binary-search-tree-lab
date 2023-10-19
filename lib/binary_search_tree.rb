9a79e646bb89908908fcf10e8747fe5189c3f6de
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-10-19 11:57:29 -0600

    commit tree 1392dfe2b43064ffd27f831b727c6d4f132130d1

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -8,11 +8,49 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+    curr = @root
+
+     return nil if @root.nil?
+    
+    while curr.value != value 
+        if value > curr.value
+          curr.right = curr 
+        else value < curr.value
+          curr.left = curr 
+        end 
+      return curr 
+    end        
   end
 
   def insert(value)
-    # your code here
-  end
+    new_node = Node.new(value)
+    curr = @root
+
+    if @root.nil?
+      new_node = @root
+      return @root
+    end
+
+    curr?
+    if curr.value == value
+      return nil
+    end
+
+    if value > curr.value
+      if current.right.nil?
+        curr.right = new_node
+        return new_node
+      end
+      curr = curr.right
+    elsif value < curr.value
+      if current.left.nil?
+        curr.left = new_node
+        return new_node
+      end
+      curr = curr.left
+      
+    end
+  end 
+
 
 end 
